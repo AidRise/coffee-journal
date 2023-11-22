21b62f2f135d90a38e8d476438b967ae2231ea18
Aidan Reising <aidan.reising@gmail.com>
2023-11-22 13:44:38 -0600

    Initial client app setup

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello from the server!!');
+    res.send('Hello from the server for the coffee journal!!');
 });
 
 app.listen(8080, () => {
