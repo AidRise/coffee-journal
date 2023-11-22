b9a538bfbd05b2477dcc8a2aba6f7edd4e7441b9
Aidan Reising <aidan.reising@gmail.com>
2023-11-22 17:37:04 -0600

    Update server to enable cors

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello from the server for the coffee journal!!');
